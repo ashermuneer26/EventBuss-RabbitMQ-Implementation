b5ccd637746a7367bf23bf2b57adeba943d6ceb2
ashermuneer <50703858+ashermuneer@users.noreply.github.com>
2021-02-25 21:29:11 +0500

    some changes

---
+++ AdvantageControlApplicationModule.cs
@@ -0,0 +1,40 @@
+﻿using Abp.AutoMapper;
+using Abp.Events.Bus;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Castle.MicroKernel.Registration;
+using MTR.EventBus.Shared;
+
+namespace AdvantageControl
+{
+    [DependsOn(
+        typeof(AdvantageControlCoreModule), 
+        typeof(AbpAutoMapperModule),
+        typeof(EventBusSharedModule)
+    )]
+    public class AdvantageControlApplicationModule : AbpModule
+    {
+        public override void PreInitialize()
+        {
+            Configuration.BackgroundJobs.IsJobExecutionEnabled = false;
+            Configuration.ReplaceService(
+                typeof(IEventBus),
+                () => IocManager.IocContainer.Register(
+                    Component.For<IEventBus>().Instance(NullEventBus.Instance)
+                )
+            );
+        }
+
+        public override void Initialize()
+        {
+            var thisAssembly = typeof(AdvantageControlApplicationModule).GetAssembly();
+
+            IocManager.RegisterAssemblyByConvention(thisAssembly);
+
+            Configuration.Modules.AbpAutoMapper().Configurators.Add(
+                // Scan the assembly for classes which inherit from AutoMapper.Profile
+                cfg => cfg.AddMaps(thisAssembly)                 
+            );
+        }
+    }
+}
